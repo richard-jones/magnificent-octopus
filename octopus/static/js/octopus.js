61578d65c1e238c195c5bbccfbadb00808b25323
Richard Jones <richard@cottagelabs.com>
2014-11-20 19:07:12 +0000

    tweak standard css and add our first utils to the octopus js lib

--- octopus/static/js/octopus.js
+++ octopus/static/js/octopus.js
@@ -1,1 +1,19 @@
-var octopus = {}
+var octopus = {
+    display : {
+        hidden : {},
+
+        hideOffScreen : function(selector) {
+            var el = $(selector);
+            if (selector in this.hidden) { return }
+            this.hidden[selector] = {"position" : el.css("position"), "margin" : el.css("margin-left")};
+            $(selector).css("position", "absolute").css("margin-left", -9999);
+        },
+
+        bringIn : function(selector) {
+            var pos = this.hidden[selector].position;
+            var mar = this.hidden[selector].margin;
+            $(selector).css("position", pos).css("margin-left", mar);
+            delete this.hidden[selector];
+        }
+    }
+};
