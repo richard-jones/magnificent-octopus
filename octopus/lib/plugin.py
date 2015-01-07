ab95475364c8813b7e49512ef074618dae234b37
Richard Jones <richard@cottagelabs.com>
2015-01-07 17:31:21 +0000

    add plugin loader for functions

--- octopus/lib/plugin.py
+++ octopus/lib/plugin.py
@@ -27,3 +27,24 @@ def load_class(classpath, cache_class_ref=True):
 
 def load_module(modpath):
     return importlib.import_module(modpath)
+
+def load_function_raw(fnpath):
+    modpath = ".".join(fnpath.split(".")[:-1])
+    fnname = fnpath.split(".")[-1]
+    mod = importlib.import_module(modpath)
+    fn = getattr(mod, fnname)
+    return fn
+
+def load_function(fnpath, cache_fn_ref=True):
+    fn = app.config.get("PLUGIN_FN_REFS", {}).get(fnpath)
+    if fn is not None:
+        return fn
+
+    fn = load_function_raw(fnpath)
+
+    if cache_fn_ref:
+        if "PLUGIN_FN_REFS" not in app.config:
+            app.config["PLUGIN_FN_REFS"] = {}
+        app.config["PLUGIN_FN_REFS"][fnpath] = fn
+
+    return fn
