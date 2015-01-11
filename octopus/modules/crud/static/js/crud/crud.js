51e2ffce77559404793072b742de752612e64798
Richard Jones <richard@cottagelabs.com>
2015-01-11 23:44:38 +0000

    add update and retrieve crud js

--- octopus/modules/crud/static/js/crud/crud.js
+++ octopus/modules/crud/static/js/crud/crud.js
@@ -20,6 +20,46 @@ jQuery(document).ready(function($) {
                     complete: complete_callback,
                     error: error_callback
                 })
+            },
+
+            retrieve : function(params) {
+                var objtype = params.objtype;
+                var id = params.id;
+                var success_callback = params.success;
+                var complete_callback = params.complete;
+                var error_callback = params.error;
+
+                $.ajax({
+                    type: "GET",
+                    contentType: "application/json",
+                    dataType: "jsonp",
+                    url: octopus.config.crud_endpoint + "/" + objtype + "/" + id,
+                    success: success_callback,
+                    complete: complete_callback,
+                    error: error_callback
+                })
+            },
+
+            update : function(params) {
+                var dataobj = params.dataobj;
+                var objtype = params.objtype;
+                var id = params.id;
+                var success_callback = params.success;
+                var complete_callback = params.complete;
+                var error_callback = params.error;
+
+                var putdata = JSON.stringify(dataobj.data);
+
+                $.ajax({
+                    type: "PUT",
+                    contentType: "application/json",
+                    dataType: "jsonp",
+                    url: octopus.config.crud_endpoint + "/" + objtype + "/" + id,
+                    data : putdata,
+                    success: success_callback,
+                    complete: complete_callback,
+                    error: error_callback
+                })
             }
         }
     });
