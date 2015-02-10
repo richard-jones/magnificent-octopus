c3ae12b6c6256e5b2980b353335e4abe88c7e90e
Richard Jones <richard@cottagelabs.com>
2015-02-10 19:42:05 +0000

    add fragments capability to render from template

--- octopus/modules/clientjs/fragments.py
+++ octopus/modules/clientjs/fragments.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort
+from flask import Blueprint, abort, render_template
 
 from octopus.core import app
 from octopus.lib import plugin
@@ -8,8 +8,18 @@ blueprint = Blueprint('fragments', __name__)
 @blueprint.route("/<frag_id>")
 def fragment(frag_id):
     cfg = app.config.get("FRAGMENTS", {}).get(frag_id)
+    if cfg is None:
+        abort(404)
+
+    # see if we want to render from a function
     fname = cfg.get("function")
     if fname is not None:
         fn = plugin.load_function(fname)
         return fn()
+
+    # see if we want to render from a template
+    temp = cfg.get("template")
+    if temp is not None:
+        return render_template(temp)
+
     abort(404)
