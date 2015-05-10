a4f08a9451a37ce49fbba7e155ccc7db6c91787f
Richard Jones <richard@cottagelabs.com>
2015-05-10 15:26:13 +0100

    add full structural validation capabilities to data objects

--- octopus/lib/dataobj.py
+++ octopus/lib/dataobj.py
@@ -2,6 +2,93 @@ from octopus.lib import dates
 from copy import deepcopy
 import locale, json
 
+#########################################################
+## Data coerce functions
+
+def to_unicode():
+    def to_utf8_unicode(val):
+        if isinstance(val, unicode):
+            return val
+        elif isinstance(val, basestring):
+            try:
+                return val.decode("utf8", "strict")
+            except UnicodeDecodeError:
+                raise ValueError(u"Could not decode string")
+        else:
+            return unicode(val)
+
+    return to_utf8_unicode
+
+def to_int():
+    def intify(val):
+        # strip any characters that are outside the ascii range - they won't make up the int anyway
+        # and this will get rid of things like strange currency marks
+        if isinstance(val, unicode):
+            val = val.encode("ascii", errors="ignore")
+
+        # try the straight cast
+        try:
+            return int(val)
+        except ValueError:
+            pass
+
+        # could have commas in it, so try stripping them
+        try:
+            return int(val.replace(",", ""))
+        except ValueError:
+            pass
+
+        # try the locale-specific approach
+        try:
+            return locale.atoi(val)
+        except ValueError:
+            pass
+
+        raise ValueError(u"Could not convert string to int: {x}".format(x=val))
+
+    return intify
+
+def to_float():
+    def floatify(val):
+        # strip any characters that are outside the ascii range - they won't make up the float anyway
+        # and this will get rid of things like strange currency marks
+        if isinstance(val, unicode):
+            val = val.encode("ascii", errors="ignore")
+
+        # try the straight cast
+        try:
+            return float(val)
+        except ValueError:
+            pass
+
+        # could have commas in it, so try stripping them
+        try:
+            return float(val.replace(",", ""))
+        except ValueError:
+            pass
+
+        # try the locale-specific approach
+        try:
+            return locale.atof(val)
+        except ValueError:
+            pass
+
+        raise ValueError(u"Could not convert string to float: {x}".format(x=val))
+
+    return floatify
+
+def date_str(in_format=None, out_format=None):
+    def datify(val):
+        return dates.reformat(val, in_format=in_format, out_format=out_format)
+
+    return datify
+
+############################################################
+
+############################################################
+## The core data object which manages all the interactions
+## with the underlying data member variable
+
 class DataSchemaException(Exception):
     pass
 
@@ -13,8 +100,30 @@ class DataObj(object):
 
     SCHEMA = None
 
-    def __init__(self, raw=None):
+    STRUCT = None
+
+    DEFAULT_COERCE = {
+        "unicode" : to_unicode(),
+        "utcdatetime" : date_str(),
+        "integer" : to_int(),
+        "float" : to_float()
+    }
+
+    def __init__(self, raw=None, coerce=None):
+        # prep the coersion routine
+        self.coerce = deepcopy(self.DEFAULT_COERCE)
+        if coerce is not None:
+            self.coerce.update(coerce)
+
+        # restructure the object based on the struct
+        if self.STRUCT is not None:
+            raw = construct(raw, self.STRUCT, self.coerce)
+
+        # assign the data
         self.data = {} if raw is None else raw
+
+        # run against the old validation routine
+        # (now deprecated)
         self.validate()
 
     def validate(self):
@@ -227,87 +336,46 @@ class DataObj(object):
         current.append(val)
 
     def _utf8_unicode(self):
-        def to_utf8_unicode(val):
-            if isinstance(val, unicode):
-                return val
-            elif isinstance(val, basestring):
-                try:
-                    return val.decode("utf8", "strict")
-                except UnicodeDecodeError:
-                    raise ValueError(u"Could not decode string")
-            else:
-                return unicode(val)
-
-        return to_utf8_unicode
+        """
+        DEPRECATED - use dataobj.to_unicode() instead
+        """
+        return to_unicode()
 
     def _int(self):
-        def intify(val):
-            # strip any characters that are outside the ascii range - they won't make up the float anyway
-            # and this will get rid of things like strange currency marks
-            if isinstance(val, unicode):
-                val = val.encode("ascii", errors="ignore")
-
-            # try the straight cast
-            try:
-                return int(val)
-            except ValueError:
-                pass
-
-            # could have commas in it, so try stripping them
-            try:
-                return int(val.replace(",", ""))
-            except ValueError:
-                pass
-
-            # try the locale-specific approach
-            try:
-                return locale.atoi(val)
-            except ValueError:
-                pass
-
-            raise ValueError(u"Could not convert string to int: {x}".format(x=val))
-
-        return intify
+        """
+        DEPRECATED - use dataobj.to_int() instead
+        """
+        return to_int()
 
     def _float(self):
-        def floatify(val):
-            # strip any characters that are outside the ascii range - they won't make up the float anyway
-            # and this will get rid of things like strange currency marks
-            if isinstance(val, unicode):
-                val = val.encode("ascii", errors="ignore")
-
-            # try the straight cast
-            try:
-                return float(val)
-            except ValueError:
-                pass
-
-            # could have commas in it, so try stripping them
-            try:
-                return float(val.replace(",", ""))
-            except ValueError:
-                pass
-
-            # try the locale-specific approach
-            try:
-                return locale.atof(val)
-            except ValueError:
-                pass
-
-            raise ValueError(u"Could not convert string to float: {x}".format(x=val))
-
-        return floatify
+        """
+        DEPRECATED - use dataobj.to_float() instead
+        """
+        return to_float()
 
     def _date_str(self, in_format=None, out_format=None):
-        def datify(val):
-            return dates.reformat(val, in_format=in_format, out_format=out_format)
+        """
+        DEPRECATED - use dataobj.date_str instead
+        """
+        return date_str(in_format=in_format, out_format=out_format)
 
-        return datify
+
+
+############################################################
+## Primitive object schema validation
 
 class ObjectSchemaValidationError(Exception):
     pass
 
+
 def validate(obj, schema):
+    """
+    DEPRECATED - use "construct" instead
+
+    :param obj:
+    :param schema:
+    :return:
+    """
     # all fields
     allowed = schema.get("bools", []) + schema.get("fields", []) + schema.get("lists", []) + schema.get("objects", [])
 
@@ -353,6 +421,134 @@ def validate(obj, schema):
                 pass # we are not imposing a schema on this object
             else:
                 validate(v, object_schema)
+
+############################################################
+## Data structure coercion
+
+class DataStructureException(Exception):
+    pass
+
+def construct(obj, struct, coerce, context=""):
+    """
+    {
+        "fields" : {
+            "field_name" : {"coerce" :"coerce_function"}
+
+        },
+        "objects" : [
+            "field_name"
+        ],
+        "lists" : {
+            "field_name" : {"contains" : "object|list|field", "coerce" : "field_coerce_function}
+        },
+        "reqired" : ["field_name"],
+        "structs" : {
+            "field_name" : {
+                <construct>
+            }
+        }
+    }
+
+    :param obj:
+    :param struct:
+    :param coerce:
+    :return:
+    """
+    if obj is None:
+        return None
+
+    # check that all the required fields are there
+    keys = obj.keys()
+    for r in struct.get("required", []):
+        if r not in keys:
+            c = context if context != "" else "root"
+            raise DataStructureException("Field '{r}' is required but not present at '{c}'".format(r=r, c=c))
+
+    # check that there are no fields that are not allowed
+    allowed = struct.get("fields", {}).keys() + struct.get("objects", []) + struct.get("lists", {}).keys()
+    for k in keys:
+        if k not in allowed:
+            c = context if context != "" else "root"
+            raise DataStructureException("Field '{k}' is not permitted at '{c}'".format(k=k, c=c))
+
+    # this is the new object we'll be creating from the old
+    constructed = {}
+
+    # now check all the fields
+    for field_name, instructions in struct.get("fields", {}).iteritems():
+        val = obj.get(field_name)
+        if val is None:
+            continue
+        coerce_fn = coerce.get(instructions.get("coerce", "unicode"))
+        if coerce_fn is None:
+            raise DataStructureException("No coersion function defined for type '{x}' at '{c}'".format(x=instructions.get("coerce", "unicode"), c=context + field_name))
+
+        try:
+            constructed[field_name] = coerce_fn(val)
+        except ValueError as e:
+            raise DataStructureException("Unable to coerce '{v}' to '{fn}' at '{c}'".format(v=val, fn=instructions.get("coerce", "unicode"), c=context + field_name))
+
+    # next check all the objetcs (which will involve a recursive call to this function
+    for field_name in struct.get("objects", []):
+        val = obj.get(field_name)
+        if val is None:
+            continue
+        if type(val) != dict:
+            raise DataStructureException("Found '{x}' = '{y}' but expected object/dict".format(x=context + field_name, y=val))
+
+        instructions = struct.get("struct", {}).get(field_name)
+        if instructions is None:
+            constructed[field_name] = deepcopy(val)
+        else:
+            constructed[field_name] = construct(val, instructions, coerce=coerce, context=context + field_name + ".")
+
+    # now check all the lists
+    for field_name, instructions in struct.get("lists", {}).iteritems():
+        vals = obj.get(field_name)
+        if vals is None:
+            continue
+
+        nvals = []
+        contains = instructions.get("contains")
+        if contains == "field":
+            # coerce all the values in the list
+            coerce_fn = coerce.get(instructions.get("coerce", "unicode"))
+            if coerce_fn is None:
+                raise DataStructureException("No coersion function defined for type '{x}' at '{c}'".format(x=instructions.get("coerce", "unicode"), c=context + field_name))
+
+            for i in xrange(len(vals)):
+                val = vals[i]
+                try:
+                    nvals.append(coerce_fn(val))
+                except ValueError as e:
+                    raise DataStructureException("Unable to coerce '{v}' to '{fn}' at '{c}' position '{p}'".format(v=val, fn=instructions.get("coerce", "unicode"), c=context + field_name, p=i))
+
+        elif contains == "object":
+            # for each object in the list, send it for construction
+            for i in range(len(vals)):
+                val = vals[i]
+
+                if type(val) != dict:
+                    raise DataStructureException("Found '{x}[{p}]' = '{y}' but expected object/dict".format(x=context + field_name, y=val, p=i))
+
+                subinst = struct.get("struct", {}).get(field_name)
+                if subinst is None:
+                    nvals.append(deepcopy(val))
+                else:
+                    nvals.append(construct(val, subinst, coerce=coerce, context=context + field_name + "[" + str(i) + "]."))
+
+        else:
+            raise DataStructureException("Cannot understand structure where list '{x}' elements contain '{y}'".format(x=context + field_name, y=contains))
+
+        constructed[field_name] = nvals
+
+    return constructed
+
+
+
+
+############################################################
+## Unit test support
 
 def test_dataobj(obj, fields_and_values):
     """
--- octopus/tests/unit/test_dataobj.py
+++ octopus/tests/unit/test_dataobj.py
@@ -13,3 +13,136 @@ class TestImport(TestCase):
         d._set_single("one.two", "value")
         d._set_single("one.three.four.five", "value")
         d._delete("one.three.four.five")
+
+    def test_02_construct(self):
+        struct = {
+            "fields" : {
+                "one" : {"coerce" : "unicode"}
+            },
+            "objects" : [
+                "three", "four", "five"
+            ],
+            "lists" : {
+                "six" : {"contains" : "whatever"},
+                "seven" : {"contains" : "field", "coerce" : "integer"},
+                "eight" : {"contains" : "object"},
+                "nine" : {"contains" : "object"}
+            },
+            "required" : ["one"],
+            "struct" : {
+                # Note there's no structure for three, so it can be anything
+                "four" : {
+                    "fields" : {
+                        "alpha" : {"coerce" : "integer"}
+                    }
+                },
+                "nine" : {
+                    "fields" : {
+                        "beta" : {"coerce" : "integer"}
+                    }
+                }
+            }
+        }
+
+        coerce = {
+            "unicode" : dataobj.to_unicode(),
+            "integer" : dataobj.to_int()
+        }
+
+        obj = { "one" : "hello" }
+        new = dataobj.construct(obj, struct, coerce)
+        assert new["one"] == u"hello"
+        assert isinstance(new["one"], unicode)
+
+        # try adding a disallowed field
+        obj["two"]  = "world"
+
+        with self.assertRaises(dataobj.DataStructureException):
+            try:
+                new = dataobj.construct(obj, struct, coerce)
+            except dataobj.DataStructureException as e:
+                print e.message
+                raise e
+
+        # try not providing a required field
+        obj = {}
+        with self.assertRaises(dataobj.DataStructureException):
+            try:
+                new = dataobj.construct(obj, struct, coerce)
+            except dataobj.DataStructureException as e:
+                print e.message
+                raise e
+
+        # try an unvalidated sub-object
+        obj = {"one" : "hello"}
+        obj["three"] = {"something" : "here"}
+        new = dataobj.construct(obj, struct, coerce)
+        assert new["three"]["something"] == "here"
+
+        # and a validated sub-object
+        obj["four"] = {"alpha" : "4"}
+        new = dataobj.construct(obj, struct, coerce)
+        assert new["four"]["alpha"] == 4
+
+        # and a field that should be an object but isn't
+        obj["five"] = "something here"
+        with self.assertRaises(dataobj.DataStructureException):
+            try:
+                new = dataobj.construct(obj, struct, coerce)
+            except dataobj.DataStructureException as e:
+                print e.message
+                raise e
+
+        # a list where the list contains directive is broken
+        del obj["five"]
+        obj["six"] = ["6"]
+        with self.assertRaises(dataobj.DataStructureException):
+            try:
+                new = dataobj.construct(obj, struct, coerce)
+            except dataobj.DataStructureException as e:
+                print e.message
+                raise e
+
+        # successfully coerce a list of field values
+        del obj["six"]
+        obj["seven"] = ["1", "1", "2", "3"]
+        new = dataobj.construct(obj, struct, coerce)
+        assert new["seven"] == [1, 1, 2, 3]
+
+        # faile to coerce a list of field values
+        obj["seven"] = ["a", "b", "walton-upon-thames"]
+        with self.assertRaises(dataobj.DataStructureException):
+            try:
+                new = dataobj.construct(obj, struct, coerce)
+            except dataobj.DataStructureException as e:
+                print e.message
+                raise e
+
+        # list is supposed to contain an object but doesn't
+        del obj["seven"]
+        obj["eight"]  = ["not an object"]
+        with self.assertRaises(dataobj.DataStructureException):
+            try:
+                new = dataobj.construct(obj, struct, coerce)
+            except dataobj.DataStructureException as e:
+                print e.message
+                raise e
+
+        # list contains an object but no validation required on its structure
+        obj["eight"] = [{"an" : "object"}]
+        new = dataobj.construct(obj, struct, coerce)
+        assert new["eight"][0]["an"] == "object"
+
+        # list contains an object which fails to validate
+        obj["nine"] = [{"beta" : "whatever"}]
+        with self.assertRaises(dataobj.DataStructureException):
+            try:
+                new = dataobj.construct(obj, struct, coerce)
+            except dataobj.DataStructureException as e:
+                print e.message
+                raise e
+
+        # list contains an object that does validate
+        obj["nine"] = [{"beta" : 9}]
+        new = dataobj.construct(obj, struct, coerce)
+        assert new["nine"][0]["beta"] == 9
