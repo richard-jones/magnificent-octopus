1602f09842eb05152f9cfe58130f5835022817ef
Richard Jones <richard@cottagelabs.com>
2015-07-02 11:27:39 +0100

    add first parts of jper client

---
+++ octopus/modules/jper/client.py
@@ -0,0 +1,113 @@
+from octopus.core import app
+from octopus.modules.jper import models
+from octopus.lib import http
+import json
+
+class JPERException(Exception):
+    pass
+
+class ValidationException(Exception):
+    pass
+
+class JPER(object):
+
+    FilesAndJATS = "http://router.jisc.ac.uk/packages/FilesAndJATS"
+
+    def __init__(self, api_key=None, base_url=None):
+        self.api_key = api_key if api_key is not None else app.config.get("JPER_API_KEY")
+        self.base_url = base_url if base_url is not None else app.config.get("JPER_BASE_URL")
+
+    def _url(self, endpoint=None, auth=True):
+        url = self.base_url
+        if not url.endswith("/"):
+            url += "/"
+
+        if endpoint is not None:
+            url += endpoint
+
+        if auth:
+            url += "?api_key=" + http.quote(self.api_key)
+
+        return url
+
+    def validate(self, notification, file_handle=None):
+        # turn the notification into a json string
+        data = None
+        if isinstance(notification, models.IncomingNotification):
+            data = notification.json()
+        else:
+            data = json.dumps(notification)
+
+        # get the url that we are going to send to
+        url = self._url("validate")
+
+        resp = None
+        if file_handle is None:
+            # if there is no file handle supplied, send the metadata-only notification
+            resp = http.post(url, data=data, headers={"Content-Type" : "application/json"})
+        else:
+            # otherwise send both parts as a multipart message
+            files = [
+                ("metadata", ("metadata.json", data, "application/json")),
+                ("content", ("content.zip", file_handle, "application/zip"))
+            ]
+            resp = http.post(url, files=files)
+
+        if resp is None:
+            raise JPERException("Unable to communicate with the JPER API")
+
+        if resp.status_code == 401:
+            raise JPERException("Could not authenticate with JPER with your API key")
+
+        if resp.status_code == 400:
+            raise ValidationException(resp.json().get("error"))
+
+        return True
+
+    def create_notification(self, notification, file_handle=None):
+        # turn the notification into a json string
+        data = None
+        if isinstance(notification, models.IncomingNotification):
+            data = notification.json()
+        else:
+            data = json.dumps(notification)
+
+        # get the url that we are going to send to
+        url = self._url("notification")
+
+        resp = None
+        if file_handle is None:
+            # if there is no file handle supplied, send the metadata-only notification
+            resp = http.post(url, data=data, headers={"Content-Type" : "application/json"})
+        else:
+            # otherwise send both parts as a multipart message
+            files = [
+                ("metadata", ("metadata.json", data, "application/json")),
+                ("content", ("content.zip", file_handle, "application/zip"))
+            ]
+            resp = http.post(url, files=files)
+
+        if resp is None:
+            raise JPERException("Unable to communicate with the JPER API")
+
+        if resp.status_code == 401:
+            raise JPERException("Could not authenticate with JPER with your API key")
+
+        if resp.status_code == 400:
+            raise ValidationException(resp.json().get("error"))
+
+        # extract the useful information from the acceptance response
+        acc = resp.json()
+        id = acc.get("id")
+        loc = acc.get("location")
+
+        return id, loc
+
+    def get_notification(self, notification_id):
+        return None
+
+    def list_notifications(self, since, page=None, page_size=None, repository_id=None):
+        return None
+
+    def record_retrieval(self, notification_id, content_id=None):
+        pass
---
+++ octopus/modules/jper/settings.py
@@ -0,0 +1,6 @@
+# Base URL for requests to the JPER API
+JPER_BASE_URL = "http://router.jisc.ac.uk/api/v1"
+
+# API key to use for authenticated requests against JPER API
+JPER_API_KEY = ""
+
