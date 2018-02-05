4b67e92864365cacbf57c03fe8d988f6ca59442b
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-02-05 16:04:45 +0100

    catch other DOI link forms as well

--- octopus/modules/identifiers/doi.py
+++ octopus/modules/identifiers/doi.py
@@ -4,7 +4,7 @@ def normalise(doi):
 
     doi = doi.strip()
 
-    rx = "^((http:\/\/){0,1}dx.doi.org/|(http:\/\/){0,1}hdl.handle.net\/|doi:|info:doi:){0,1}(?P<id>10\\..+\/.+)"
+    rx = "^((http(s){0,1}:\/\/){0,1}(dx.){0,1}doi.org/|(http:\/\/){0,1}hdl.handle.net\/|doi:|info:doi:){0,1}(?P<id>10\\..+\/.+)"
     result = re.match(rx, doi)
     if result is None:
         raise ValueError(doi + " does not seem to be a valid DOI")
