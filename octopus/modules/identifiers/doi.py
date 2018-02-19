e0f476b36dde7baa985d3af47272479df110af9b
Steven Eardley <steve@eardley.xyz>
2018-02-19 21:24:45 +0000

    Updated and simplified DOI URL based on efforts from @katrinlienweber

--- octopus/modules/identifiers/doi.py
+++ octopus/modules/identifiers/doi.py
@@ -4,7 +4,7 @@ def normalise(doi):
 
     doi = doi.strip()
 
-    rx = "^((http:\/\/){0,1}dx.doi.org/|(http:\/\/){0,1}hdl.handle.net\/|doi:|info:doi:){0,1}(?P<id>10\\..+\/.+)"
+    rx = r"^((https?:\/\/)?((dx\.)?doi\.org/|hdl\.handle\.net\/)|doi:|info:doi:)?(?P<id>10\..+\/.+)"
     result = re.match(rx, doi)
     if result is None:
         raise ValueError(doi + " does not seem to be a valid DOI")
